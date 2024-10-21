f533468b32ba378d00e326a2c45bffb84f4ce557
Brendon King <bking@calltower.com>
2024-10-21 16:46:05 -0600

    Begin work on v1.0.1, with menu strip, icons, and improved outBox

--- NumberBlocker/Form1.Designer.cs
+++ NumberBlocker/Form1.Designer.cs
@@ -36,20 +36,26 @@
             this.txtTenantId = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
             this.groupBox2 = new System.Windows.Forms.GroupBox();
-            this.radioEnabled = new System.Windows.Forms.RadioButton();
-            this.radioDisabled = new System.Windows.Forms.RadioButton();
-            this.txtPattern = new System.Windows.Forms.TextBox();
-            this.label2 = new System.Windows.Forms.Label();
+            this.btnSubmit = new System.Windows.Forms.Button();
             this.label3 = new System.Windows.Forms.Label();
             this.txtDescription = new System.Windows.Forms.TextBox();
-            this.btnSubmit = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtPattern = new System.Windows.Forms.TextBox();
+            this.radioDisabled = new System.Windows.Forms.RadioButton();
+            this.radioEnabled = new System.Windows.Forms.RadioButton();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.helpToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.aboutToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.groupBox1.SuspendLayout();
             this.groupBox2.SuspendLayout();
+            this.menuStrip1.SuspendLayout();
             this.SuspendLayout();
             // 
             // outBox
             // 
-            this.outBox.Location = new System.Drawing.Point(308, 12);
+            this.outBox.Location = new System.Drawing.Point(308, 27);
             this.outBox.Multiline = true;
             this.outBox.Name = "outBox";
             this.outBox.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
@@ -62,7 +68,7 @@
             this.groupBox1.Controls.Add(this.btnConnect);
             this.groupBox1.Controls.Add(this.txtTenantId);
             this.groupBox1.Controls.Add(this.label1);
-            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Location = new System.Drawing.Point(12, 27);
             this.groupBox1.Name = "groupBox1";
             this.groupBox1.Size = new System.Drawing.Size(290, 83);
             this.groupBox1.TabIndex = 1;
@@ -116,51 +122,22 @@
             this.groupBox2.Controls.Add(this.radioDisabled);
             this.groupBox2.Controls.Add(this.radioEnabled);
             this.groupBox2.Enabled = false;
-            this.groupBox2.Location = new System.Drawing.Point(12, 101);
+            this.groupBox2.Location = new System.Drawing.Point(12, 116);
             this.groupBox2.Name = "groupBox2";
             this.groupBox2.Size = new System.Drawing.Size(290, 193);
             this.groupBox2.TabIndex = 2;
             this.groupBox2.TabStop = false;
             this.groupBox2.Text = "Number Blocking Details";
             // 
-            // radioEnabled
+            // btnSubmit
             // 
-            this.radioEnabled.AutoSize = true;
-            this.radioEnabled.Location = new System.Drawing.Point(6, 19);
-            this.radioEnabled.Name = "radioEnabled";
-            this.radioEnabled.Size = new System.Drawing.Size(58, 17);
-            this.radioEnabled.TabIndex = 3;
-            this.radioEnabled.TabStop = true;
-            this.radioEnabled.Text = "Enable";
-            this.radioEnabled.UseVisualStyleBackColor = true;
-            // 
-            // radioDisabled
-            // 
-            this.radioDisabled.AutoSize = true;
-            this.radioDisabled.Enabled = false;
-            this.radioDisabled.Location = new System.Drawing.Point(70, 19);
-            this.radioDisabled.Name = "radioDisabled";
-            this.radioDisabled.Size = new System.Drawing.Size(60, 17);
-            this.radioDisabled.TabIndex = 1;
-            this.radioDisabled.TabStop = true;
-            this.radioDisabled.Text = "Disable";
-            this.radioDisabled.UseVisualStyleBackColor = true;
-            // 
-            // txtPattern
-            // 
-            this.txtPattern.Location = new System.Drawing.Point(56, 42);
-            this.txtPattern.Name = "txtPattern";
-            this.txtPattern.Size = new System.Drawing.Size(228, 20);
-            this.txtPattern.TabIndex = 4;
-            // 
-            // label2
-            // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(6, 45);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(44, 13);
-            this.label2.TabIndex = 3;
-            this.label2.Text = "Pattern:";
+            this.btnSubmit.Location = new System.Drawing.Point(209, 156);
+            this.btnSubmit.Name = "btnSubmit";
+            this.btnSubmit.Size = new System.Drawing.Size(75, 23);
+            this.btnSubmit.TabIndex = 6;
+            this.btnSubmit.Text = "Submit";
+            this.btnSubmit.UseVisualStyleBackColor = true;
+            this.btnSubmit.Click += new System.EventHandler(this.btnSubmit_Click);
             // 
             // label3
             // 
@@ -179,33 +156,105 @@
             this.txtDescription.Size = new System.Drawing.Size(228, 82);
             this.txtDescription.TabIndex = 5;
             // 
-            // btnSubmit
+            // label2
             // 
-            this.btnSubmit.Location = new System.Drawing.Point(209, 156);
-            this.btnSubmit.Name = "btnSubmit";
-            this.btnSubmit.Size = new System.Drawing.Size(75, 23);
-            this.btnSubmit.TabIndex = 6;
-            this.btnSubmit.Text = "Submit";
-            this.btnSubmit.UseVisualStyleBackColor = true;
-            this.btnSubmit.Click += new System.EventHandler(this.btnSubmit_Click);
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(6, 45);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(44, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Pattern:";
+            // 
+            // txtPattern
+            // 
+            this.txtPattern.Location = new System.Drawing.Point(56, 42);
+            this.txtPattern.Name = "txtPattern";
+            this.txtPattern.Size = new System.Drawing.Size(228, 20);
+            this.txtPattern.TabIndex = 4;
+            // 
+            // radioDisabled
+            // 
+            this.radioDisabled.AutoSize = true;
+            this.radioDisabled.Enabled = false;
+            this.radioDisabled.Location = new System.Drawing.Point(70, 19);
+            this.radioDisabled.Name = "radioDisabled";
+            this.radioDisabled.Size = new System.Drawing.Size(60, 17);
+            this.radioDisabled.TabIndex = 1;
+            this.radioDisabled.TabStop = true;
+            this.radioDisabled.Text = "Disable";
+            this.radioDisabled.UseVisualStyleBackColor = true;
+            // 
+            // radioEnabled
+            // 
+            this.radioEnabled.AutoSize = true;
+            this.radioEnabled.Location = new System.Drawing.Point(6, 19);
+            this.radioEnabled.Name = "radioEnabled";
+            this.radioEnabled.Size = new System.Drawing.Size(58, 17);
+            this.radioEnabled.TabIndex = 3;
+            this.radioEnabled.TabStop = true;
+            this.radioEnabled.Text = "Enable";
+            this.radioEnabled.UseVisualStyleBackColor = true;
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem,
+            this.helpToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(510, 24);
+            this.menuStrip1.TabIndex = 3;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.exitToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // helpToolStripMenuItem
+            // 
+            this.helpToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.aboutToolStripMenuItem});
+            this.helpToolStripMenuItem.Name = "helpToolStripMenuItem";
+            this.helpToolStripMenuItem.Size = new System.Drawing.Size(44, 20);
+            this.helpToolStripMenuItem.Text = "Help";
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.exitToolStripMenuItem.Text = "Exit";
+            // 
+            // aboutToolStripMenuItem
+            // 
+            this.aboutToolStripMenuItem.Name = "aboutToolStripMenuItem";
+            this.aboutToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.aboutToolStripMenuItem.Text = "About";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(510, 306);
+            this.ClientSize = new System.Drawing.Size(510, 322);
             this.Controls.Add(this.groupBox2);
             this.Controls.Add(this.groupBox1);
             this.Controls.Add(this.outBox);
+            this.Controls.Add(this.menuStrip1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MainMenuStrip = this.menuStrip1;
             this.MaximizeBox = false;
             this.Name = "Form1";
-            this.Text = "Teams Number Blocker v1.0.0";
+            this.Text = "Teams Number Blocker v1.0.1";
             this.groupBox1.ResumeLayout(false);
             this.groupBox1.PerformLayout();
             this.groupBox2.ResumeLayout(false);
             this.groupBox2.PerformLayout();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -227,6 +276,11 @@
         private System.Windows.Forms.TextBox txtDescription;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.TextBox txtPattern;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem helpToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem aboutToolStripMenuItem;
     }
 }
 
